5a5b29c736cbbcb50215e59acb892c74858ae846
Unayung Chen <unayung@gmail.com>
2019-12-24 12:33:28 +0800

    fix

--- lib/capistrano/messaging/telegram.rb
+++ lib/capistrano/messaging/telegram.rb
@@ -8,45 +8,33 @@ module Capistrano
       extend Forwardable
       def_delegators :env, :fetch
 
-      def rocket
-        '%F0%9F%9a%80'
-      end
-
-      # def bang
-      #   '%E2%80%BC%EF%B8%8F'
-      # end
-
-      def robo
-        '%F0%9F%A4%96'
-      end
-
       def payload_for_updating
         {
-          text: "#{robo} #{deployer} has started deploying branch #{branch} of #{application} to #{stage}"
+          text: "#{deployer} has started deploying branch #{branch} of #{application} to #{stage}"
         }
       end
 
       def payload_for_reverting
         {
-          text: "#{robo} #{deployer} has started rolling back branch #{branch} of #{application} to #{stage}"
+          text: "#{deployer} has started rolling back branch #{branch} of #{application} to #{stage}"
         }
       end
 
       def payload_for_updated
         {
-          text: "#{rocket} #{deployer} has finished deploying branch #{branch} of #{application} to #{stage}"
+          text: "#{deployer} has finished deploying branch #{branch} of #{application} to #{stage}"
         }
       end
 
       def payload_for_reverted
         {
-          text: "#{robo} #{deployer} has finished rolling back branch of #{application} to #{stage}"
+          text: "#{deployer} has finished rolling back branch of #{application} to #{stage}"
         }
       end
 
       def payload_for_failed
         {
-          text: "#{robo} #{deployer} has failed to #{deploying? ? 'deploy' : 'rollback'} branch #{branch} of #{application} to #{stage}"
+          text: "#{deployer} has failed to #{deploying? ? 'deploy' : 'rollback'} branch #{branch} of #{application} to #{stage}"
         }
       end
 
